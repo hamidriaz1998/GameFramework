5c59d95ca2b1120b12de350f7f48542d3a251cb2
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-30 13:25:23 +0500

    Implement GameObject class with gravity support

---
+++ GameObject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameFramework
+{
+    internal class GameObject
+    {
+        private PictureBox Pb;
+        public bool gravity;
+        public PictureBox GetPb()
+        {
+            return Pb;
+        }
+        public GameObject(Image img,int top,int left)
+        {
+            Pb = new PictureBox
+            {
+                Image = img,
+                Top = top,
+                Left = left,
+                BackColor = Color.Transparent,
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+        }
+        public virtual void move(int gravity)
+        {
+            Pb.Top += gravity;
+        }
+    }
+}
