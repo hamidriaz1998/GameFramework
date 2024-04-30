4de03a07f295c0df1bce295e2be580fed4caa9c3
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-30 07:22:24 +0500

    Complete gravity implementation

--- Game.cs
+++ Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,33 @@ namespace GameFramework
     {
         private Form GameForm;
         private int Gravity;
+        private List<PictureBox> GravityObjects;
         public Game(Form form,int gravity)
         {
             GameForm = form;
             Gravity = gravity;
+            GravityObjects = new List<PictureBox>();
+        }
+        public void addGameObject(Image image, int top, int left, bool gravity)
+        {
+            PictureBox picture = new PictureBox();
+            picture.Image = image;
+            picture.Top = top;
+            picture.Left = left;
+            picture.BackColor = Color.Transparent;
+            picture.SizeMode = PictureBoxSizeMode.StretchImage;
+            GameForm.Controls.Add(picture);
+            if (gravity)
+            {
+                GravityObjects.Add(picture);
+            }
+        }
+        public void update()
+        {
+            foreach (PictureBox picture in GravityObjects)
+            {
+                picture.Top += Gravity;
+            }
         }
     }
 }
