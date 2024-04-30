2e646b41dc0f541b03b9c693ff84616c5a72a8fd
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-30 07:02:44 +0500

    Add project files.

---
+++ Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace GameFramework
+{
+    public class Game
+    {
+        private Form GameForm;
+        private int Gravity;
+        public Game(Form form,int gravity)
+        {
+            GameForm = form;
+            Gravity = gravity;
+        }
+    }
+}
