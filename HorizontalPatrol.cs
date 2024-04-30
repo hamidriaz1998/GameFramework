0ec4eb7db933a7bb2e91353d1bd1ef13a6510423
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-04-30 14:07:29 +0500

    Add HorizontalPatrol class for game object movement

---
+++ HorizontalPatrol.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GameFramework
+{
+    public class HorizontalPatrol: IMovement
+    {
+        private int Speed;
+        private Point Boundary;
+        private Directions Direction;
+        private int Offset = 90;
+        public HorizontalPatrol(int speed, Point boundary, Directions direction)
+        {
+            Speed = speed;
+            Boundary = boundary;
+            Direction = direction;
+        }
+        public Point Move(Point location)
+        {
+            if ((location.X + Offset) >= Boundary.X)
+            {
+                Direction = Directions.Left;
+            }
+            else if (location.X - Speed <= 0)
+            {
+                Direction = Directions.Right;
+            }
+            if (Direction == Directions.Right)
+            {
+                location.X += Speed;
+            }
+            else
+            {
+                location.X -= Speed;
+            }
+            return location;
+        }
+
+    }
+}
