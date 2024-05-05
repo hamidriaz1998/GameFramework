a6ba15896cb002951fea111d19393e6d6a49be9d
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-05-05 08:44:50 +0500

    Add Collision Detection Class

---
+++ CollisionDetection/CollisionDetection.cs
@@ -0,0 +1,42 @@
+﻿using GameFramework.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework.CollisionDetection
+{
+    public class CollisionDetection
+    {
+        private GameObjectType Type1;
+        private GameObjectType Type2;
+        private CollisionAction Action;
+        public CollisionDetection(GameObjectType type1, GameObjectType type2, CollisionAction action)
+        {
+            Type1 = type1;
+            Type2 = type2;
+            Action = action;
+        }
+        public void CheckCollision(List<GameObject> gameObjects)
+        {
+            var type1Objects = gameObjects.Where(go => go.Type == Type1);
+            var type2Objects = gameObjects.Where(go => go.Type == Type2);
+            foreach (var go1 in type1Objects)
+            {
+                foreach (var go2 in type2Objects)
+                {
+                    if (go1.Pb.Bounds.IntersectsWith(go2.Pb.Bounds))
+                    // TODO: Implement collision action
+                    {
+                        if (Action == CollisionAction.IncreaseHealth)
+                        {
+                            
+
+                        }   
+                    }
+                }
+            }
+        }
+    }
+}
