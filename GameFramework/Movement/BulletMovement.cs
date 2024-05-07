622195f52fc039416e5a19d3a6595027051aa71a
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-05-07 21:35:37 +0500

    Add Bullet Movement

---
+++ GameFramework/Movement/BulletMovement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFramework.Movement
+{
+    internal class BulletMovement : IMovement
+    {
+        private int Speed;
+        private Directions Direction;
+        public BulletMovement(int speed, Directions direction)
+        {
+            Speed = speed;
+            Direction = direction;
+        }
+        public Point Move(Point location)
+        {
+            if (Direction == Directions.Up)
+            {
+                location.Y -= Speed;
+            }
+            else if (Direction == Directions.Down)
+            {
+                location.Y += Speed;
+            }
+            else if (Direction == Directions.Left)
+            {
+                location.X -= Speed;
+            }
+            else
+            {
+                location.X += Speed;
+            }
+            return location;
+        }
+    }
+}
