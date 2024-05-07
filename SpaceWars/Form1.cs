9826d8e4c733653c10c5e6fb5f0272ecc7d5e457
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-05-07 10:33:53 +0500

    Change Folder structure and add Game project to the solution

---
+++ SpaceWars/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GameFramework;
+
+namespace SpaceWars
+{
+    public partial class Form1 : Form
+    {
+        private Game game;
+        private Point Boundary;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = Screen.PrimaryScreen.WorkingArea.Width;
+            this.Height = Screen.PrimaryScreen.WorkingArea.Height;
+            Boundary = new Point(this.Width, this.Height);
+            game = Game.GetInstance(this);
+        }
+
+        private void GameLoop_Tick(object sender, EventArgs e)
+        {
+            game.Update();
+        }
+    }
+}
