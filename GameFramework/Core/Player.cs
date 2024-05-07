a29f1e75f03cd6c735407e5d35e59aa53b60a87c
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-05-07 20:06:54 +0500

    Add Player class

---
+++ GameFramework/Core/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameFramework.Core
+{
+    public class Player : Character
+    {
+        public Player(Image img, int top, int left, IMovement controller, GameObjectType type, ProgressBar HealthBar, Label label, Image fireImage) : base(img, top, left, controller, type, HealthBar, label, fireImage)
+        {
+            var KeyController = (KeyboardHandler)controller;
+            KeyController.SetPlayer(this);
+        }
+
+        public override void Fire()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
